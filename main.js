79c4da6ce9d9c93b9ed6ef3432f5ff99f792c793
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-01-21 15:23:40 -0500

    rel tags added

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
+$('a').click(function(event){
+  event.preventDefault();
 
+  var relatedClass = "." + $(this).attr('rel');
+console.log(relatedClass);
+});
 
 
 
 });
+
+
+//insteat of $(.post3) do $(relatedClass)
