72a38108c19d75575bc54f89d026d4a23974069e
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-01-22 16:12:40 -0500

    figuring out functionality to get back to album from full page image

--- main.js
+++ main.js
@@ -10,16 +10,50 @@ $('a').click(function(event){
   $(relatedClass).addClass('active');
   $('nav').addClass('active');
   $(this).parent().removeClass('active');
-  $('.image').html('<img src="'+$(this).data('photo')+'">').addClass('active');
+
+
 
 
   });
-//   $('img').click(function(event){
-//     event.preventDefault();
-//     $('nav').removeClass('active');
+
+
+
+
+  $('article a').click(function(event){
+    event.preventDefault();
+    $('nav').removeClass('active');
+    $('.image').html('<img src="'+$(this).data('photo')+'">').addClass('active');
+    $('.image').siblings('h3').addClass('active').attr("data-album", $(this).data('album'));
+    $('.image').parent('.lgImage').addClass('active');
+  });
+
+  $('h3').click(function(event){
+    event.preventDefault();
+    $(this).removeClass('active');
+    $(this).siblings().removeClass('active');
+    $(this).parent().removeClass('active');
+    $("article").child().attr("data-album", $(this).data("." + 'album')).addClass('active');
+  });
+
+  $('.image').hover(function(event){
+    event.preventDefault();
+    $(this).css('transform', 'scale(1.5)' );
+    $(this).css('transform-origin', '0 0');
+
+
+  });
+//
+// $('.image img').click(function(event){
+//   event.preventDefault();
+//
+//   $(this).removeClass('active');
+//   $(this).
+//   ('nav').addClass('active');
+//
 //
 // });
+//
+
+
+
 });
-
-
-//insteat of $(.post3) do $(relatedClass)
